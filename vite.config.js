e1a2382bb6c0865a63948835047b7368ccfc163b
RbzEric <jelseric@gmail.com>
2026-08-18 22:35:09 +0300

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,48 +2,46 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// 🔥 detect raha Electron
-const isElectron = process.env.ELECTRON === "true";
-
 export default defineConfig({
-  base: "./",
+  base: '/PHARMACIE-APP/',
 
   plugins: [
     react(),
 
-    // 🔥 PWA mandeha raha TSY Electron
-    !isElectron &&
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['icon-192.png', 'icon-512.png'],
-        manifest: {
-          name: 'Pharmacie App',
-          short_name: 'Pharma',
-          description: 'Gestion pharmacie offline',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
-          display: 'standalone',
-          start_url: './',
-          icons: [
-            {
-              src: './icon-192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: './icon-512.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        },
-        workbox: {
-          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-        }
-      })
-  ].filter(Boolean), // 🔥 manala false
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon-192.png', 'icon-512.png'],
+
+      manifest: {
+        name: 'Pharmacie App',
+        short_name: 'Pharma',
+        description: 'Gestion pharmacie offline',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/PHARMACIE-APP/',
+
+        icons: [
+          {
+            src: '/PHARMACIE-APP/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/PHARMACIE-APP/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wasm}']
+      }
+    })
+  ],
 
   build: {
-    outDir: "dist"
+    outDir: 'dist'
   }
 })
